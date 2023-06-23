2130481feb2d5460d3ef838c6e952ffb74b8a613
Joline Cheng <anjolinea@gmail.com>
2023-06-22 23:30:37 -0400

    added check constraint

--- use_toy_dataset.py
+++ use_toy_dataset.py
@@ -7,17 +7,27 @@ sys.path.append( '../toy_dataset' )
 from toy_dataset.use_toy_dataset_util import *
 from load_toy_dataset import load_toy_dataset
 
-test_name = "recommend_new_for_two"
-
-in_query_folder = "sample_queries/in/"
-in_query_file = test_name + ".sql"
-in_file = in_query_folder + in_query_file
-
-out_query_folder = "sample_queries/out/"
-out_query_file = test_name + ".out"
-out_file = out_query_folder + out_query_file
-
 connection = sqlite3.connect('database.db')
 load_toy_dataset(connection)
 
-run_command_from_file(connection=connection, input_filename=in_file, output_filename=out_file)
+## --- for playing around with SQL files -----
+# test_name = "recommend_new_for_two"
+
+# in_query_folder = "sample_queries/in/"
+# in_query_file = test_name + ".sql"
+# in_file = in_query_folder + in_query_file
+
+# out_query_folder = "sample_queries/out/"
+# out_query_file = test_name + ".out"
+# out_file = out_query_folder + out_query_file
+
+# run_command_from_file(connection=connection, input_filename=in_file, output_filename=out_file)
+## ------------------
+
+query = """
+INSERT INTO Follows(userID1, userID2)
+VALUES ('charliewatanabe59', 'charliewatanabe59');
+"""
+
+## a CHECK constraint violation should happen!
+run_command_from_string(connection, query, True)
